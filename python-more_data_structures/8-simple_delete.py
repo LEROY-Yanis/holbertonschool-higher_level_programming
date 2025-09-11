d1f924f1e0064a741b3063cb6f43972abcf1b532
LEROY-Yanis <11479@holbertonstudents.com>
2025-09-11 12:46:17 +0200

    refactor: clean up comments in simple_delete function

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
-    if key in a_dictionary:   # on vérifie si la clé existe
-        del a_dictionary[key] # on supprime la clé
-    return a_dictionary       # on retourne le dictionnaire
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
