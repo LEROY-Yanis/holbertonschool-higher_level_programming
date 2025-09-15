264fd996cd264a5c8516db8e33b9ea644d20380a
LEROY-Yanis <11479@holbertonstudents.com>
2025-09-15 11:53:16 +0200

    fix: update docstrings for clarity in Square class

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-# Define a class called Square
+"""
+Define a class called Square
+"""
 class Square:
-    # Constructor method to initialize the square
+    """
+    Constructor method to initialize the square
+    """
     def __init__(self, size):
-        # Private attribute to store the size of the square
-        # (Note: here it's hardcoded to 3, ignoring the 'size' parameter)
+        """
+        Private attribute to store the size of the square
+        (Note: here it's hardcoded to 3, ignoring the 'size' parameter)
+        """
         self.__size__ = size
