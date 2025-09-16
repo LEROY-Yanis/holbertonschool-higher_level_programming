1a48c6b03e48d5cd81b738499b71451dd87eae05
LEROY-Yanis <11479@holbertonstudents.com>
2025-09-16 13:03:51 +0200

    feat: create Rectangle class with initial documentation for square geometry

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module square
+This module defines a Square class to work with basic square geometry.
+"""
+
+
+class Rectangle:
+    """
+    A class that represents a square.
+    """
+    pass
