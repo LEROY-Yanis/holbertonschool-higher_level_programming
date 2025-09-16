62c3316d4c89cead3505d44c6c232793ae4d929b
LEROY-Yanis <11479@holbertonstudents.com>
2025-09-16 14:03:31 +0200

    feat: redefine Rectangle class with width and height attributes and validation

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,31 +1,42 @@
 #!/usr/bin/python3
 """
-Define a class called Rectangle
+Defines a class called Rectangle
 """
 
 
 class Rectangle:
-
     """
-    Constructor method with a default size of 0
+    A class that defines a rectangle by width and height
     """
-    def __init__(self, size=0):
 
-        """
-        Check if 'size' is an integer
-        """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
 
-        """
-        Check if 'size' is non-negative
-        """
+    @property
+    def width(self):
+        """Retrieve the width"""
+        return self.__width
 
-        if size < 0:
-            raise ValueError("size must be >= 0")
+    @width.setter
+    def width(self, value):
+        """Set the width with validation"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-        """
-        Store the validated size as a private attribute
-        """
+    @property
+    def height(self):
+        """Retrieve the height"""
+        return self.__height
 
-        self.__size = size
+    @height.setter
+    def height(self, value):
+        """Set the height with validation"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
