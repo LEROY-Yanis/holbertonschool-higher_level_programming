d6b141cce09e8f3f031a44da1d1beece8fcccda0
LEROY-Yanis <11479@holbertonstudents.com>
2025-09-23 13:24:35 +0200

    style: format docstring for inherits_from function for improved readability

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,9 +4,12 @@ Module 1-my_list
 Defines a class MyList that inherits from list
 """
 
+
 def inherits_from(obj, a_class):
-	"""
-	Returns True if obj is an instance of a class that inherited from a_class
-	(directly or indirectly), but not if obj is an instance of a_class itself.
-	"""
-	return isinstance(obj, a_class) and type(obj) is not a_class
+
+    """
+    Returns True if obj is an instance of a class that inherited from a_class
+    (directly or indirectly), but not if obj is an instance of a_class itself.
+    """
+
+    return isinstance(obj, a_class) and type(obj) is not a_class
