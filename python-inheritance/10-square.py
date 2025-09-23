afcddf38ed1fca82ce0aa4fada0c3d101a01e61c
LEROY-Yanis <11479@holbertonstudents.com>
2025-09-23 15:45:59 +0200

    feat: add Square class inheriting from Rectangle with size validation

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""BaseGeometry class with area and integer_validator methods"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square that inherits from Rectangle"""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
