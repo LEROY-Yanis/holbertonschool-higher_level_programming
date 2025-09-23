6de171553b75caa5f40651d1d938812a4c6f266a
LEROY-Yanis <11479@holbertonstudents.com>
2025-09-23 14:50:37 +0200

    style: update module docstring for clarity in BaseGeometry class

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
+"""
+Module 1-my_list
+Defines a class MyList that inherits from list
+"""
+
+
 class BaseGeometry:
 	"""BaseGeometry class with area and integer_validator methods"""
 
