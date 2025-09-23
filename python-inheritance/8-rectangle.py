6feaa20f8926aefc38331b46b31edb0592decdbb
LEROY-Yanis <11479@holbertonstudents.com>
2025-09-23 15:19:30 +0200

    style: fix indentation and formatting in Rectangle class

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,12 +4,13 @@
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-class Rectangle(BaseGeometry):
-	"""Rectangle that inherits from BaseGeometry"""
 
-	def __init__(self, width, height):
-		super().__init__()
-		self.integer_validator("width", width)
-		self.integer_validator("height", height)
-		self.__width = width
-		self.__height = height
+class Rectangle(BaseGeometry):
+    """Rectangle that inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        super().__init__()
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
