901c4d22a51aeab6161339934eb1cd2e09c28646
LEROY-Yanis <11479@holbertonstudents.com>
2025-09-30 09:36:47 +0200

    feat: implement pascal_triangle function to generate Pascal's triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+"""
+Module that provides a function to return the JSON representation
+of an object (string).
+"""
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing
+    the Pascal's triangle of size n.
+    Each list inside the main list represents a row
+    of Pascal's triangle.
+    """
+    if n <= 0:
+        return []
+    triangle = []
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                row.append(prev_row[j - 1] + prev_row[j])
+        triangle.append(row)
+        prev_row = row
+    return triangle
