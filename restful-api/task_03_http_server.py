e0cdbc154471eda82c5f2d06fbe0818e6327da44
LEROY-Yanis <11479@holbertonstudents.com>
2025-10-09 13:31:39 +0200

    fix: correct content type for /status endpoint response

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -23,12 +23,13 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
 
         elif self.path == '/status':
             self.send_response(200)
-            self.send_header('Content-type', 'application/json')
+            self.send_header('Content-type', 'text/html')
             self.end_headers()
-            self.wfile.write(b"yes")
+            self.wfile.write(b"OK")
 
         else:
             self.send_response(404)
+            self.send_header('Content-type', 'text/html')
             self.end_headers()
             self.wfile.write(b"404 Not Found")
 
