e0c447a526a28031b3ce7033eb9807df2ad48533
LEROY-Yanis <11479@holbertonstudents.com>
2025-10-09 14:00:41 +0200

    fix: update 404 response message for clarity

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -31,7 +31,7 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"404 Not Found")
+            self.wfile.write(b"Endpoint not found")
 
 
 def run(port=8000):
