258662839fcae4887f93403ed4876da607fda31f
LEROY-Yanis <11479@holbertonstudents.com>
2025-10-10 16:30:46 +0200

    feat: enhance Flask API with user management and improved response handling

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -6,14 +6,18 @@ from flask import make_response
 from flask import request
 
 
+HOST = '0.0.0.0'
 PORT = 5000
+
+USERS = {}
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home(self):
-    return "Welcome to the Flask API!"
+    return "Welcome to the Flask API!", 200
 
 
 @app.route('/data')
@@ -29,10 +33,46 @@ def status():
 
 
 @app.route('/users/<username>')
+def user(username):
+    user = USERS.get(username)
+    if user:
+        return jsonify(user), 200
+    else:
+        return jsonify({"error": "User not found"}), 404
 
 
 @app.route('/users')
+def users():
+    return jsonify(USERS), 200
+
+
+@app.route('/add_user', methods=['POST'])
+def add_users():
+    user_data = request.get_json()
+
+    if not user_data:
+        return jsonify({"error": "Username is required"}), 400
+    
+    if 'username' not in user_data:
+        return jsonify({"error": "Username is required"}), 400
+
+    username = user_data['username']
+
+    USERS[username] = user_data
+
+    return jsonify({
+        "message": "User added",
+        "user": user_data
+    }), 201
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({"error": "Endpoint not found"}), 404)
 
 
 if __name__ == "__main__":
-    app.run()
+    print(f"Starting Flask server on http://localhost:{PORT}...")
+    print(f"(Accessible via {HOST}:{PORT})")
+
+    app.run(host=HOST, port=PORT, debug=True)
