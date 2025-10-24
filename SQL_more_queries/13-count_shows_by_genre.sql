e8fd6836434b342bd1facf714cb1c2e7d95368b1
LEROY-Yanis <11479@holbertonstudents.com>
2025-10-24 15:16:43 +0000

    count shows by genre

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,2 +1,10 @@
- 
+-- Lists all genres and the number of shows linked to each one
+
+SELECT g.name AS genre,
+       COUNT(sg.show_id) AS number_of_shows
+FROM tv_genres AS g
+JOIN tv_show_genres AS sg ON g.id = sg.genre_id
+GROUP BY g.name
+HAVING COUNT(sg.show_id) > 0
+ORDER BY number_of_shows DESC;
 
