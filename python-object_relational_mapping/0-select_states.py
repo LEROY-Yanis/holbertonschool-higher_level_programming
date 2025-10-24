86932c98a8f851883c2f3325433295e0de6d8189
LEROY-Yanis <11479@holbertonstudents.com>
2025-10-24 21:44:31 +0200

    refactor: rewrite script to list all states from the hbtn_0e_0_usa database

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,29 @@
--- Create states table in hbtn_0e_0_usa with some data
-CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
-USE hbtn_0e_0_usa;
-CREATE TABLE IF NOT EXISTS states ( 
-    id INT NOT NULL AUTO_INCREMENT, 
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
-);
-INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
+#!/usr/bin/python3
+"""Script that lists all states from the database hbtn_0e_0_usa.
+Usage: ./0-select_states.py username password database_name
+"""
+import MySQLdb
+import sys
 
+
+if __name__ == "__main__":
+    # Connect to the mysql database
+    db = MySQLdb.connect(
+        port=3306,
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+        )
+    # Create a cursor object
+    cursor = db.cursor()
+    # Execute the sql query to fetch all states ordered by id
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    # Fetch all results
+    query_rows = cursor.fetchall()
+    # Print each row (state)
+    for row in query_rows:
+        print(row)
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
