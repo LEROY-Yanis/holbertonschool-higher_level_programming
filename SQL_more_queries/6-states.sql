ee56167459c56ff4dcc26dcdfb9ba997e375c8a5
LEROY-Yanis <11479@holbertonstudents.com>
2025-10-24 12:54:18 +0000

    states

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- Creates database hbtn_0d_usa and table states with id as primary key.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL);
+
