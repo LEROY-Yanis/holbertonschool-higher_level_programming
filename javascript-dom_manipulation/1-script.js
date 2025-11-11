3b707233fc20925115b3b0e5dfc9d7b35aa9ea9f
LEROY-Yanis <11479@holbertonstudents.com>
2025-11-11 14:33:13 +0100

    feat: add 1-script.js to update header text color to red on click

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,4 @@
+// Updates the text color of the header to red when clicking on red_header
+document.querySelector('#red_header').addEventListener('click', function() {
+  document.querySelector('header').style.color = '#FF0000';
+});
