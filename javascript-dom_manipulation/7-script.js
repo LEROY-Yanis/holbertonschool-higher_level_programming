5eb61bd6727024a145a8e1a21450eedce287bdf9
LEROY-Yanis <11479@holbertonstudents.com>
2025-11-11 14:33:42 +0100

    feat: add 7-script.js to fetch and list all Star Wars movie titles

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,11 @@
+// Fetches and lists all Star Wars movie titles
+fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+  .then(response => response.json())
+  .then(data => {
+    const moviesList = document.querySelector('#list_movies');
+    data.results.forEach(movie => {
+      const listItem = document.createElement('li');
+      listItem.textContent = movie.title;
+      moviesList.appendChild(listItem);
+    });
+  });
