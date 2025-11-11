72e574a9c3ae6e99bd11bae0a42497d1ee12a3f4
LEROY-Yanis <11479@holbertonstudents.com>
2025-11-11 14:33:18 +0100

    feat: add 2-script.js to add red class to header on click

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,4 @@
+// Adds the class red to the header element when clicking on red_header
+document.querySelector('#red_header').addEventListener('click', function() {
+  document.querySelector('header').classList.add('red');
+});
