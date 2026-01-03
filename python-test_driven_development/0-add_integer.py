93de00823e2a51f6aaee44357fe66dc29590a2ae
LEROY-Yanis <11479@holbertonstudents.com>
2026-01-03 15:13:52 +0100

    fix: refine type checks in add_integer function to handle infinity cases

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,8 +10,12 @@ def add_integer(a, b=98):
     """Adds two integers and returns the result.
     Floats are casted to integers before addition.
     """
-    if not isinstance(a, (int, float)) or a != a or a == float('inf') or a == float('-inf'):
+    if not isinstance(a, (int, float)) or a != a:
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)) or b != b or b == float('inf') or b == float('-inf'):
+    if a == float('inf') or a == float('-inf'):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)) or b != b:
+        raise TypeError("b must be an integer")
+    if b == float('inf') or b == float('-inf'):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
