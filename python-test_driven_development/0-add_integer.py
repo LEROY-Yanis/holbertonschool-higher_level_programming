7ab36cd093405a1b5e8b38ceec3ee092f3f7e651
LEROY-Yanis <11479@holbertonstudents.com>
2026-01-03 14:41:48 +0100

    add: implement add_integer function with type checks and create test cases

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module provides a function to add two integers.
+a and b must be integers or floats.
+Floats are casted to integers before addition.
+"""
+
+
+def add_integer(a, b=98):
+    """Adds two integers and returns the result.
+    Floats are casted to integers before addition.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
