19667281a2370d9d895fa38322516e68e78fa04a
LEROY-Yanis <11479@holbertonstudents.com>
2026-02-12 14:39:47 +0100

    add: implement MyClass with string representation and JSON serialization support

--- python-input_output/8-my_class_to_json.py
+++ python-input_output/8-my_class_to_json.py
@@ -3,6 +3,13 @@
 
 
 def class_to_json(obj):
-    """Returns the dictionary description with simple data structure
-    for JSON serialization of an object."""
+    """Return the dictionary description with simple data structure
+    for JSON serialization of an object.
+
+    Args:
+        obj: An instance of a Class with serializable attributes.
+
+    Returns:
+        A dictionary representation of obj's attributes.
+    """
     return obj.__dict__
