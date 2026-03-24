4354250cc3845dcfe51b782a6912ace037214c99
LEROY-Yanis <11479@holbertonstudents.com>
2026-03-24 17:47:32 +0100

    fix: standardize indentation in multi_c.js and square.js for consistency

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -2,12 +2,10 @@
 
 const size = parseInt(process.argv[2]);
 
-if (Number.isNaN(size))
-{
+if (Number.isNaN(size)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < size; i++)
-{
+  for (let i = 0; i < size; i++) {
     console.log('X'.repeat(size));
-}
+  }
 }
