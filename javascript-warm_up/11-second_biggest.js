e5a819c0b7eff50e996120f56d25356c1f7b4f06
LEROY-Yanis <11479@holbertonstudents.com>
2026-03-25 12:47:52 +0100

    add: create second_biggest.js, object.js, and add.js with initial implementations

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const numbers = process.argv.slice(2).map(Number);
+
+if (numbers.length < 2) {
+    console.log(0);
+} else {
+    const sorted = numbers.sort((a, b) => b - a);
+    console.log(sorted[1]);
+}
